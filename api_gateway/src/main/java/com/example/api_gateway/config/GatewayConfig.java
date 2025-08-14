14ca3fa9fdee9b2192e93ed2af7242b8aaf8830f
HieuNguyenCode <hieu88815@gmail.com>
2025-08-14 07:24:12 +0700

    đang làm buoi 7

---
+++ api_gateway/src/main/java/com/example/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,23 @@
+package com.example.api_gateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class GatewayConfig {
+    @Bean
+    public RouteLocator customFilter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service", r -> r.path("/api/v1/auth/**")
+                        .uri("lb://auth-service"))
+                .route("account-service", r -> r.path("/api/v1/accounts/**")
+                        .uri("lb://account-service"))
+                .route("department-service", r -> r.path("/api/v1/departments/**")
+                        .uri("lb://department-service"))
+                .build();
+    }
+}
